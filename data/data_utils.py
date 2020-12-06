732cd3c29cc5c57dc57511cad048a5a341ad2fbf
j-huthmacher <email@jens-huthmacher.de>
2020-12-06 19:07:39 +0100

    Add data converter, add loss extraction from DB

--- data/data_utils.py
+++ data/data_utils.py
@@ -2,6 +2,10 @@
 
     @author: jhuthmacher
 """
+from typing import Any
+
+import numpy as np
+
 from data.tig_data_set import TIGDataset
 
 
@@ -20,3 +24,53 @@ def get_max_frames(dataset: TIGDataset):
         max_frames = max_frames if max_frames > data.frames else data.frames
 
     return max_frames
+
+def convert_data(data: Any, to="float32"):
+    """ Converts the data types in the data to the specified type.
+
+        Important: If you provide a path in "data" the old files will be replaced.
+    """
+
+    if isinstance(data, str):
+        ####  data is a path ####
+        path = data
+        data = np.load(data, allow_pickle=True)
+        data = data["data"]
+
+        np.savez(path.replace(".npz", ""),
+                 x=np.asarray(np.asarray(data)[:, 0].tolist()).astype(to),
+                 y=np.asarray(np.asarray(data)[:, 1].tolist()).astype(to))        
+    else:
+        return [np.asarray(np.asarray(data)[:, 0].tolist()).astype(to),
+                np.asarray(np.asarray(data)[:, 1].tolist()).astype(to)]
+
+
+
+########################
+# Load Results from DB #
+########################
+
+def get_loss(db_url: str, experiment: dict):
+    """
+        Example: experiment  = {"run_id": 85, "name": {"$regex": "MLP\..*\.loss\.epoch\..*"} }
+    """
+    client = pymongo.MongoClient(db_url)
+    collection = client.temporal_info_graph["metrics"]
+    cursor = collection.find(experiment)
+
+    trains_loss = []
+    val_loss = []
+    val_top1 = []
+    val_top5 = []
+
+    for doc in cursor:        
+        if "loss" in doc["name"] and "train" in doc["name"]:
+            trains_loss.append((doc["steps"], doc["values"]))
+        elif "loss" in doc["name"] and "val" in doc["name"]:
+            val_loss.append((doc["steps"], doc["values"]))
+        elif "top1" in doc["name"] and "val" in doc["name"]:
+            val_top1.append((doc["steps"], doc["values"]))
+        elif "top5" in doc["name"] and "val" in doc["name"]:
+            val_top5.append((doc["steps"], doc["values"]))
+    
+    return trains_loss, val_loss, val_top1, val_top5

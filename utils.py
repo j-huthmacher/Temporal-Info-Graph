72ee1840fe313e66db042611f4a90642d65e38d8
j-huthmacher <email@jens-huthmacher.de>
2020-12-09 11:25:49 +0100

    Fix type issue, Add numpy to utils

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 """
     @author: jhuthmacher
 """
+import numpy as np
 
 from config.config import log
 
-def print_size(tensor):
+def print_size(tensor, byte_per_element):
     """
     """
-    log.info("Size Unit", (np.prod(tensor.shape) * 3) // 10**6, "MB")
+    log.info("Size Unit", (np.prod(tensor.shape) * byte_per_element) // 10**6, "MB")

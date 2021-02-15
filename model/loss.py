6265901e0250fb3748e859dcb39e028941355ab1
j-huthmacher <j.huthmacher@campus.lmu.de>
2021-02-15 19:42:16 +0100

    Add hypersphere loss

--- model/loss.py
+++ model/loss.py
@@ -11,6 +11,33 @@ import torch.nn.functional as F
 
 matplotlib.use('Agg')
 
+def hypersphere_loss(enc_global: torch.Tensor, enc_local: torch.Tensor):
+    """
+    """
+    self = hypersphere_loss
+
+    self.num_graphs = enc_global.shape[0]
+    self.num_nodes = enc_local.shape[-1]
+    self.dim = enc_global.shape[1]
+
+    # positive graph samples
+    x = enc_global.repeat(self.num_nodes, 1)
+    # corresponding local samples
+    y = enc_local.permute(0,2,1).reshape(-1,self.dim)
+
+    def align_loss(x, y, alpha=2):
+        """
+            (x,y) corresponds to a positive pair
+        """
+        return (x - y).norm(p=2, dim=1).pow(alpha).mean()
+
+    def uniform_loss(x, t=2):
+        return torch.pdist(x, p=2).pow(2).mul(-t).exp().mean().log()
+    lam = 0.5
+    loss = align_loss(x, y) + lam * (uniform_loss(x) + uniform_loss(y)) / 2
+
+    return loss
+
 
 def bce_loss(enc_global: torch.Tensor, enc_local: torch.Tensor):
     """ Binary cross entropy loss with preceeding discriminator.
@@ -48,10 +75,12 @@ def bce_loss(enc_global: torch.Tensor, enc_local: torch.Tensor):
     # Diagonal with blocks 'num_nodes' ones on the diagonal. Labels the date
     # with 1 for positive sample and 0 for negative sample.
     self.mask = torch.block_diag(*[torch.ones(self.num_nodes) for _ in range(self.num_graphs)]).to("cpu")
+
     num_neg_samples = self.num_graphs * self.num_nodes  * (self.num_graphs - 1)
     num_pos_samples = self.num_graphs * self.num_nodes
-
     self.b_xent = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([num_neg_samples/num_pos_samples]))
+
+    self.dim = enc_global.shape[1]
 
     flat_disc = self.discr_matr.flatten()
     flat_mask = self.mask.flatten()
@@ -90,19 +119,19 @@ def jensen_shannon_mi(enc_global: torch.Tensor, enc_local: torch.Tensor):
     # Diagonal with blocks 'num_nodes' ones on the diagonal
     self.mask = torch.block_diag(*[torch.ones(self.num_nodes) for _ in range(self.num_graphs)]).to("cpu")
 
-    self.pos_samples = self.discr_matr[self.mask == 1].reshape(self.num_graphs, self.num_nodes)
-    self.neg_samples = self.discr_matr[self.mask == 0].reshape(self.num_graphs,
+    self.pos_samples = self.discr_matr[self.mask == 1].view(self.num_graphs, self.num_nodes)
+    self.neg_samples = self.discr_matr[self.mask == 0].view(self.num_graphs,
                                                           (self.num_graphs - 1) * self.num_nodes)
 
     #### Expectation Calculation ####
     # Normalize with the size of the batch to get the batch loss.
     self.E_pos = get_positive_expectation(self.pos_samples, average=False).sum()
-    self.E_pos = (self.E_pos / (self.num_nodes * self.num_graphs)) 
+    # self.E_pos = (self.E_pos / (self.num_nodes * self.num_graphs)) 
     self.E_neg = get_negative_expectation(self.neg_samples, average=False).sum()
-    self.E_neg = (self.E_neg / (self.num_nodes * (self.num_graphs - 1) * self.num_graphs))
+    # self.E_neg = (self.E_neg / (self.num_nodes * (self.num_graphs - 1) * self.num_graphs))
 
     #### Actual Loss Calculation ####
-    return self.E_neg - self.E_pos
+    return (self.E_neg/(self.num_nodes * (self.num_graphs - 1) * self.num_graphs)) - (self.E_pos/(self.num_nodes * self.num_graphs))
 
 
 def get_positive_expectation(p_samples: torch.Tensor, measure: str = "JSD", average: bool = True):

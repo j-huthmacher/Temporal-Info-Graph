8a9abc50952d89e44930fb138085800e09e8a979
j-huthmacher <j.huthmacher@campus.lmu.de>
2021-02-22 19:08:54 +0100

    Clean MLP import

--- model/__init__.py
+++ model/__init__.py
@@ -3,10 +3,9 @@
     @author: jhuthmacher
 """
 
-from .solver import Solver
-from .loss import jensen_shannon_mi, get_negative_expectation, get_positive_expectation, bce_loss
-from .temporal_info_graph import TemporalInfoGraph, TemporalConvolution, SpectralConvolution
-from .tig_lstm import TemporalInfoGraphLSTM
-from .mlp import MLP
+from model.solver import Solver
+from model.loss import jensen_shannon_mi, get_negative_expectation, get_positive_expectation, bce_loss
+from model.temporal_info_graph import TemporalInfoGraph, TemporalConvolution, SpectralConvolution
+from model.tig_lstm import TemporalInfoGraphLSTM
 
 

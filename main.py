c1f7fde2a24554d64155aef6d67c37b25137f7f2
j-huthmacher <j.huthmacher@campus.lmu.de>
2021-02-27 11:34:59 +0100

    Simplified main procedure

--- main.py
+++ main.py
@@ -88,51 +88,31 @@ else:
         name += "_standard"
         config = yaml.load(file, Loader=yaml.FullLoader)["standard"]
 
-    for i in range(5):
-        print("Iteration", i)
-        #### Tracker Set Up ####
-        if "name" in config:
-            name += f"_{config['name']}_iteration_{i}"
-        tracking = {"ex_name": name}
-        if args.tracking == "remote":
-            tracking = {
-                "ex_name": name,
-                "db_url": db_url,
-                "interactive": True
-            }
+#### Execution ####
+if args.train:
+    if "name" in config:
+        name += f"_{config['name']}"
 
-        # Training is executed from here
-        if "tracking" in config:
-            tracker = Tracker(**{**tracking, **config["tracking"]})
-        else:
-            tracker = Tracker(**tracking)
+    # For reproducibility
+    torch.manual_seed(0)
+    config["seed"] = 0
 
-        # For reproducibility
-        torch.manual_seed(0)
-        config["seed"] = 0
+    #### Tracking Location ####
+    date = datetime.now().strftime("%d%m%Y_%H%M")
+    path = f"./output/{date}_{name}/"
+    Path(path).mkdir(parents=True, exist_ok=True)
 
-        try:
-            # experiment is the template function that is executed by the tracker and configured by "config"
-            tracker.track(experiment, config)
+    log = create_logger(path)
 
+    if args.model == "tig":
+        train_tig(config, path)
+    elif args.model == "stgcn":
+        train_stgcn(config, path)
+    else:
+        log.info(f"Model not found ({args.model })!")
+    
+    log.info(f"Training done. Output path: {path}")
 
-        except Exception as e:
-            log.exception(e)
-            tracker.cfg["status"] = "Failed (Exception)"
-            tracker.cfg["end_time"] = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-            tracker.cfg["duration"] = str(datetime.strptime(tracker.cfg["end_time"],
-                                                            "%d.%m.%Y %H:%M:%S") -
-                                        datetime.strptime(tracker.cfg["start_time"],
-                                                            "%d.%m.%Y %H:%M:%S"))
-            if tracker.local:
-                tracker.track_locally()
-            path = os.path.normpath(tracker.local_path)
-            for retry in range(100):
-                try:
-                    os.rename(path, path.replace(path.split(os.sep)[-1], "FAILED_"+path.split(os.sep)[-1]))
-                    break
-                except:
-                    pass
 
 elif args.eval:
     torch.cuda.empty_cache()
@@ -166,12 +146,6 @@ elif args.eval:
 
 
 elif args.prep_data:
-    # pass
-    # Prepare data 
-    # data_paths = [
-    #     "C:/Users/email/Documents/Studium/LMU/5_Semester/Masterthesis/Datasets/Kinetics-skeleton/kinetics-skeleton/kinetics_train/",
-    #     "C:/Users/email/Documents/Studium/LMU/5_Semester/Masterthesis/Datasets/Kinetics-skeleton/kinetics-skeleton/kinetics_val/"
-    # ]
 
     data_paths = [
         "D:/Temporal Info Graph/kinetics-skeleton/kinetics_train/",
@@ -193,19 +167,18 @@ elif args.baseline:
     data = TIGDataset(name=args.data, path="../content/")
     data.x = data.x.reshape(data.x.shape[0], -1)
 
+
     top5 = []
     top1 = []
     print("Start baseline training")    
     for i in trange(10):
-        x_train, x_test, y_train, y_test = train_test_split(data.x, data.y)
-
         model = get_model("svm")
-        model.fit(x_train, y_train)
+        model.fit(data.x, data.y)
 
-        yhat = model.decision_function(x_test)                
+        yhat = model.decision_function(data.x)                
         # accuracy_score(emb_y, yhat)
-        top5.append(top_k_accuracy_score(y_test, yhat, k = 5))
-        top1.append(top_k_accuracy_score(y_test, yhat, k = 1))
+        top5.append(top_k_accuracy_score(data.y, yhat, k = 5))
+        top1.append(top_k_accuracy_score(data.y, yhat, k = 1))
         print(f"Iteration {i} | Top1: {np.mean(top1)} Top5: {np.mean(top5)} ")
 
     print("Save baseline accuracies...")

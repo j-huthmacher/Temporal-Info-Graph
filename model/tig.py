c161031bff22810b35e2ab3b5e40ff29754a905b
j-huthmacher <j.huthmacher@campus.lmu.de>
2021-02-28 21:08:48 +0100

    Add edge weighting and multi scales

--- model/tig.py
+++ model/tig.py
@@ -11,6 +11,8 @@ import torch.nn.functional as F
 
 from data.data_utils import get_normalized_adj, pad_zero_mask, pad_zero_idx
 
+from model.st_gcn_aaai18.stgcn_graph import Graph
+
 class ConstZeroLayer(torch.nn.Module):
     def forward(self, x):
         return torch.tensor([0])
@@ -72,19 +74,16 @@ class TemporalConvolution(nn.Module):
 
 
 class SpectralConvolution(nn.Module):
-    def __init__(self, c_in: int , c_out: int = 2, kernel: Any = 1,
-                 weights: Any = None, activation: str = "LeakyReLU",
-                 debug=False, A = None):
+    def __init__(self, c_in: int , c_out: int = 2, activation: str = "LeakyReLU",A = None):
         super().__init__()
 
         self.c_in = c_in
         self.c_out = c_out
         self.A = A
 
-        # self.kernel_size = 3 # Spectral Kernel
-        self.conv = nn.Conv2d(self.c_in ,
+        self.conv = nn.Conv2d(self.c_in,
                               self.c_out,
-                              kernel_size=1)
+                              kernel_size=(1, 1))
 
         self.activation = getattr(nn, activation)()
 
@@ -93,42 +92,43 @@ class SpectralConvolution(nn.Module):
         if A is None:
             A = self.A
 
-        X = torch.einsum("ntjc, ji -> ncti", [X.permute(0, 2, 3, 1), A])
-        # In: Out: (batch_size, features, nodes, time)
-        # X = torch.matmul(X, self.W).permute(0, 3, 1, 2) # This is much more expensive than the convolutional
+        X = self.conv(X)
 
-        X = self.conv(X) # Convolutiom is about 10x faster!!!
+        X = torch.einsum('nctv,vw->nctw', (X, A))
 
-        return X
+        return X.contiguous()
 
 
 class TemporalInfoGraph(nn.Module):
     def __init__(self, architecture: list = None, A: torch.Tensor = None,
-                 mode: str = "encode"):
+                 mode: str = "encode", edge_weights: bool = True,
+                 multi_scale: bool = False, num_classes: int=50):
 
         super().__init__()
 
         self.mode = mode
+        self.multi_scale = multi_scale
 
         A = torch.tensor(get_normalized_adj(A), dtype=torch.float32).to("cuda")
-        self.register_buffer('A', torch.tensor(A))
+        self.num_nodes = A.shape[0]
+        self.register_buffer('A', A)
 
         #### Initial Data Normalization ####
-        self.num_nodes = A.shape[0]
-        self.data_norm = nn.BatchNorm1d(2*self.num_nodes)
+        self.data_norm = nn.BatchNorm1d(2*18)
 
         self.layers = []
         self.res_layers = []
         self.bn_layers = []
+        self.ms_layers = []
 
         if architecture is None:
             # Fits in 4 GB Memory
             architecture = [
-                (2, 64, 64, 64),
+                (2, 32, 64, 64),
                 (64, 128, 128, 256)
             ]
 
-        for c_in, c_out, spec_out, out  in architecture:
+        for c_in, c_out, spec_out, out in architecture:
 
             tempConv1 = TemporalConvolution(c_in=c_in, c_out=c_out, kernel=9)
             specConv = SpectralConvolution(c_in=c_out, c_out=spec_out, A=self.A)
@@ -153,14 +153,29 @@ class TemporalInfoGraph(nn.Module):
             # Final batch normalization after sandwich layer
             self.bn_layers.append(nn.BatchNorm2d(out))
 
+            if multi_scale:
+                self.ms_layers.append(nn.Linear(out, architecture[-1][-1]))
+            else:
+                self.ms_layers.append(None)
+
+
         self.convLayers = nn.Sequential(*self.layers)
         self.res_layers = nn.Sequential(*self.res_layers)
         self.bn_layers = nn.Sequential(*self.bn_layers)
+        self.ms_layers = nn.Sequential(*self.ms_layers)
+
+        if edge_weights:
+            self.edge_importance = nn.ParameterList([
+                nn.Parameter(torch.ones(self.A.size()))
+                for i in self.convLayers
+            ])
+        else:
+            self.edge_importance = [1] * len(self.convLayers)
 
         self.relu = nn.LeakyReLU()
 
         if self.mode == "classify":
-            self.fcn = nn.Conv2d(256, 50, kernel_size=1)
+            self.fcn = nn.Conv2d(256, num_classes, kernel_size=1)
         else:
             self.fcn = nn.Conv2d(out, out, kernel_size=1)
 
@@ -176,12 +191,28 @@ class TemporalInfoGraph(nn.Module):
 
         X = self.relu(X)
 
+        concat_scales = []
+
         # In: (batch_size, features, time, nodes)
-        for layer, res_layer, bn in zip(self.convLayers, self.res_layers, self.bn_layers):
+        for layer, res_layer, bn, E, ms_layer in zip(self.convLayers, self.res_layers, 
+                                                     self.bn_layers, self.edge_importance,
+                                                     self.ms_layers):
             res = res_layer(X)
-            X = layer(X)
+            X = layer[:3](X)
+            X = layer[3](X, self.A * E)
+            X = layer[4:](X)
             X = bn(X + res)
             X = self.relu(X)
+
+            if self.multi_scale:
+                # In: (batch * persons, features, time, nodes), Out:(batch * persons, features, 1, nodes)
+                Z = F.avg_pool2d(X, (X.size()[2], 1))
+                # In: (batch * persons, features, 1, nodes), Out: (batch, features, nodes)
+                Z = torch.squeeze(Z.view(N, M, -1, V).mean(dim=1))
+                # In: (batch, features, nodes), Out: (batch, outfeatures, nodes)
+                Z = ms_layer(Z.view(-1, Z.shape[1])).view(Z.shape[0], -1 ,Z.shape[2])
+
+                concat_scales.append(Z)
 
         if self.mode == "encode":
             # Average over time
@@ -192,7 +223,7 @@ class TemporalInfoGraph(nn.Module):
             # In: (batch, persons, features, 1, nodes), Out:(batch, 1, features, 1, nodes)
             X = X.view(N, M, -1, V).mean(dim=1)
 
-            return torch.squeeze(X.mean(dim=-1)), torch.squeeze(X)
+            return torch.squeeze(X.mean(dim=-1)), torch.cat([X, *concat_scales], dim=2) #torch.squeeze(X)
 
         elif self.mode == "classify":
             # Average over nodes and time

bec7249a91dc73d40380d6dd31ba85ece8ee615f
j-huthmacher <j.huthmacher@campus.lmu.de>
2021-08-03 22:06:53 +0200

    Clean code

--- evaluation.py
+++ evaluation.py
@@ -1,41 +1,51 @@
+""" This file is used for the evaluation of models and baselines.
+"""
 import json
+from datetime import datetime
 
 import torch
-from torch.utils.data import DataLoader
-import numpy as np 
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import top_k_accuracy_score, accuracy_score
-from sklearn import preprocessing
 
 from tqdm.auto import tqdm, trange
 
 from baseline import get_model
-from experiments import Experiment
 from data.tig_data_set import TIGDataset
-from processor import stgcn_metric
 
-from datetime import datetime
 
-def load_eval_data(config, path=None, num_classes=50):
-    """
+def load_eval_data(config: dict, path: str = None, num_classes: int = 50,
+                   num_samples: int = 994):
+    """ This method loads the data for evaluation based on a given data set config.
+
+        Args:
+            config: dict
+                Configuration of the experiment for which the data should be loaded.
+            path: str (optional)
+                Path from which local data should be loaded.
+            num_classe: int
+                Number of classes that should be loaded.
+            num_samples: int
+                Determines the number of samples per class. The data set will be
+                aligned to this number.
+
+        Returns:
+            (np.Array, np.Array): (x, y) where x are the features and y are the
+            corresponding labels.
     """
     if path is not None:
         # Load embeddings
         x = np.load(path + "embeddings_full.npz")["x"]
         y = np.load(path + "embeddings_full.npz")["y"]
     else:
-        # Load baseline data
-        if num_classes is not None and num_classes < 50:
-            data_cfg = {**config, **{"path": "../content/"}}
-        else:
-            data_cfg = {**config, **{"path": "../content/"}}
-            
+        data_cfg = {**config, **{"path": "../content/"}}
+
         data = TIGDataset(**data_cfg)
 
         if num_classes is not None:
             # 994 samples used for the kinetics skeleton dataset
-            train = data.stratify(num=num_classes, num_samples=994, mode=1)
+            train = data.stratify(num=num_classes, num_samples=num_samples, mode=1)
             # 632 samples are used for the NTU-RGB+D dataset
             # train = data.stratify(num=num_classes, num_samples=632, mode=1)
             x = np.array(list(np.array(train)[:, 0]))
@@ -48,7 +58,7 @@ def load_eval_data(config, path=None, num_classes=50):
 
     return x, y
 
-# TODO: Make it possible to als use a single tuple
+
 def evaluate_experiments(experiments: [tuple], baseline="mlp", repeat_baseline=5,
                          repeat_model=5, portion=1., eval_mode="model+baseline",
                          num_classes=50, unify_params=False, verbose=True,
@@ -227,28 +237,59 @@ def evaluate_experiments(experiments: [tuple], baseline="mlp", repeat_baseline=5
     return results[columns_ordered]
 
 
-def run_evaluation(x_in, y_in, baseline="svm", repeat=10, ret_model=False,
-                   verbose=True, portion=1, portion_state=None, **baseline_params):
+def run_evaluation(x_in: np.ndarray, y_in: np.ndarray, baseline: str = "svm",
+                   repeat: int = 10, ret_model: bool = False, verbose: bool = True,
+                   portion: float = 1., portion_state: int = None, **baseline_params):
+    """ Executes a concrete evaluation run.
 
+        Args:
+            x_in: np.ndarray
+                Numpy array with the input data on which the evaluation should
+                be executed.
+            y_in: np.ndarray
+                Corresponding labels to the input data in x_in as numpy array.
+            baseline: str
+                Determines which baseline is used. Options ["svm", "mlp"]
+            repeat: int
+                Number of repetitions that will be performed.
+            ret_model: bool
+                Flag to decide if the model will be returned.
+            verbose: bool
+                Flag to decide if the process should be printed to the console.
+            portion: float
+                Determines which fraction of the data is used. E.g. 1 for the
+                whole data and 0.1 for 10%.
+            portion_state: int
+                Random state when splitting the date into the portion. If None
+                the number of iteration is used for reprodiciblity.
+            **baseline_params:
+                Parameters for the sklean baseline.
+
+        Returns:
+            (top1-accuracy, top5-accuracy, -1) or
+            (top1-accuracy, top5-accuracy, num_model_coeff)
+    """
     top1 = []
     top5 = []
 
     pbar = trange(repeat, desc="Top1: - | Top2: - ", disable=(not verbose))
 
     for i in pbar:
-        
         if portion < 1:
             # x, y = x[idx], y[idx]
             # x, y = get_portion(x, y, portion)
-            _, x, _, y = train_test_split(x_in, y_in, stratify=y_in, test_size=portion, random_state=i if portion_state is None else portion_state)
+            _, x, _, y = train_test_split(x_in, y_in, stratify=y_in,
+                                          test_size=portion,
+                                          random_state=i if portion_state is None else portion_state)
         else:
             x, y = x_in, y_in
 
         try:
             x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, random_state=i)
-        except:
+        # pylint: disable=broad-except
+        except Exception:
             x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=i)
-        
+
         model = get_model(baseline, verbose=0, random_state=i, **baseline_params)
         model.fit(x_train, y_train)
 
@@ -258,18 +299,15 @@ def run_evaluation(x_in, y_in, baseline="svm", repeat=10, ret_model=False,
             yhat = model.predict_proba(x_test)
 
         try:
-            top1.append(top_k_accuracy_score(y_test, yhat, k = 1))
-            top5.append(top_k_accuracy_score(y_test, yhat, k = 5))
-        except Exception as e:
+            top1.append(top_k_accuracy_score(y_test, yhat, k=1))
+            top5.append(top_k_accuracy_score(y_test, yhat, k=5))
+        # pylint: disable=broad-except
+        except Exception:
             top1.append(accuracy_score(y_test, model.predict(x_test)))
             top5.append(-1)
 
-        pbar.set_description(f"Top1: {'{:.4f}'.format(np.mean(top1)*100)} | Top2: {'{:.4f}'.format(np.mean(top5)*100)} ")
-
-    # try:
-    #     print("Number of paramter: ", sum([len(x) for x in model.coefs_]))
-    # except:
-    #     pass
+        top1, top5 = np.mean(top1)*100, np.mean(top5)*100
+        pbar.set_description(f"Top1: {'{:.4f}'.format(top1)} | Top2: {'{:.4f}'.format(top5)}")
 
     if ret_model:
         return top1, top5, model

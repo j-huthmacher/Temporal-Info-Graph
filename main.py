8d8ea40e35c90cfa20a7fbfcdebb727a4835850a
j-huthmacher <j.huthmacher@campus.lmu.de>
2021-08-18 21:17:24 +0200

    Clean main.py

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-#pylint: disable=line-too-long
+# pylint: disable=line-too-long
 """ Main file.
 
-    @author: jhuthmacher
+    Reflects the entry point for the TIG framework and provides the CLI.
 """
-import os
 import json
-import signal
-import sys
 from pathlib import Path
-
-import argparse
-import torch
-from torch.utils.data import DataLoader
-import yaml
 from datetime import datetime
+import argparse
+
+import yaml
 import numpy as np
-
+from sklearn.metrics import top_k_accuracy_score
 from tqdm import trange
+import torch
 
-#pylint: disable=import-error
-from tracker import Tracker
-from experiments import  experiment
-# from config.config import log
-from data.tig_data_set import TIGDataset
-from experiments import Experiment
-from baseline import train_baseline, get_model
+from utils.tig_data_set import TIGDataset
+from baseline import get_model
 from processor import train_tig, train_stgcn
-
 from config.config import create_logger
-
-
-from sklearn.metrics import accuracy_score, top_k_accuracy_score
-
+from evaluation import evaluate_experiments
 
 #### Set Up CLI ####
 parser = argparse.ArgumentParser(prog='tig', description='Temporal Info Graph')
 
 #### Model/Experiment CLI ####
 parser.add_argument('--config', dest='config',
-                    help='Defines which configuration should be usd. Can be a name, .json or .yml file.')
+                    help=('Defines which configuration should be usd. Can be a name,'
+                          ' .json or .yml file. Depending on wich execution mode is selected the '
+                          'config belongs to train config or evaluation config.'))
 parser.add_argument('--name', dest='name', default="TIG_Experiment",
                     help='Name of the experiment.')
 parser.add_argument('--tracking', dest='tracking', default="remote",
@@ -47,23 +36,16 @@ parser.add_argument('--train', dest='train', action='store_true',
                     help='Flag to select trainings mode.')
 parser.add_argument('--model', dest='model', default="tig",
                     help='[tig, stgcn]')
+parser.add_argument('--eval', dest='eval',
+                    help=('Option to execute evaluation mode. It requires the path to the root of'
+                          'the experiment as argument like `--eval path/to/experiment`'))
+parser.add_argument('--model_name', dest='model_name', default="Model",
+                    help='Name of the model that is displayed in the evaluation output.')
 
-
-parser.add_argument('--downstream', dest='downstream', action='store_true',
-                    help='Flag to determine if the downstream training should be executed.')
-parser.add_argument('--disable_local_store', dest='disable_local_store', action='store_true',
-                    help='Flag to determine if the models should be locally stored. Default: Models are stored locally.')
 #### Data CLI ####
 parser.add_argument('--prep_data', dest='prep_data', action="store_true",
                     help='Prepare data.')
-#### Baseline ####
-parser.add_argument('--baseline', dest='baseline', action="store_true",
-                    help='Execute baseline')
-parser.add_argument('--data', dest='data', default="stgcn_50_classes",
-                    help='Name of the data set that should be used.')
 
-# parser.add_argument('--type', dest='type', action="str",
-#                     help='Prepare data.')
 
 args = parser.parse_args()
 
@@ -81,16 +63,16 @@ if args.config is not None:
             name = args.name
             config = json.load(file)
     else:
-        with open("./config.yml") as file:
+        with open("./config/config.yml" if args.train else "./config/config_eval.yml") as file:
             name += f"_{args.config}"
             config = yaml.load(file, Loader=yaml.FullLoader)[args.config]
 else:
     print("No config provided.")
 
-#### Execution ####
+#### Execute Training ####
 if args.train:
     if "name" in config:
-        name += f"_{config['name']}"
+        name += f"_{config['name']}"  # Only for readability
 
     # For reproducibility
     torch.manual_seed(0)
@@ -102,97 +84,62 @@ if args.train:
     Path(path_init).mkdir(parents=True, exist_ok=True)
 
     log = create_logger(path_init)
+
+    # To directly test different epochs we use an list of epochs that are subsequently executed.
     epoch_list = config["training"]["n_epochs"]
-    
+
     for epochs in epoch_list:
-        log.info(f"Epoch: {epochs}")
+        log.info("Epoch: %s", str(epochs))
+        # For each epoch create an own tracking folder.
         Path(path_init + f"{epochs}epochs").mkdir(parents=True, exist_ok=True)
 
         config["training"]["n_epochs"] = epochs
 
-        for i in range(5):
-            log.info(f"Train loop: {i}")
+        repetitions = 5
+        if "repetitions" in config:
+            repetitions = config["repetitions"]
+
+        # Each experiment is repeated five times for more meaningful results
+        for i in range(repetitions):
+            log.info("Train loop: %d", i)
             path = path_init + f"{epochs}epochs/{i}/"
             Path(path).mkdir(parents=True, exist_ok=True)
 
+            # Depending on the model different training methods are needed 
             if args.model == "tig":
                 train_tig(config, path)
             elif args.model == "stgcn":
                 train_stgcn(config, path)
             else:
-                log.info(f"Model not found ({args.model })!")
+                log.info("Model not found (%s)!", args.model)
 
-    log.info(f"Training done. Output path: {path}")
+    log.info("Training done. Output path: %s", str(path))
 
-
+#### Execute Evaluation ####
 elif args.eval:
-    torch.cuda.empty_cache()
+    results = evaluate_experiments([(args.model_name, args.eval)], **config)
 
-    name = args.name
-    config = {}
+    results.to_csv(f"{args.eval}/evaluation_{datetime.now().strftime('%d%m%Y_%H%M')}.csv")
 
-    #### Load Configuration ####
-    if args.config is not None:
-        if ".yml" in args.config or ".yaml" in args.config:
-            with open(args.config) as file:
-                name = args.name
-                config = yaml.load(file, Loader=yaml.FullLoader)
-        elif ".json" in args.config:
-            with open(args.config) as file:
-                name = args.name
-                config = json.load(file)
-        else:
-            with open("./experiments/config_repo.yml") as file:
-                name += f"_{args.config}"
-                config = yaml.load(file, Loader=yaml.FullLoader)[args.config]
-    else:
-        with open("./experiments/config_repo.yml") as file:
-            name += "_standard"
-            config = yaml.load(file, Loader=yaml.FullLoader)["standard"]
-
-    exp = Experiment(**config["exp"])
-
-    if "mode" in config and config["mode"] == "sklearn":
-        exp.evaluate_emb()
+    print(f"Results are stored at: {args.eval}evaluation_{datetime.now().strftime('%d%m%Y_%H%M')}.csv")
+    print(results[["Model", "Top-1 Accuracy", "Top-1 Std.", "Top-5 Accuracy", "Top-5 Std."]])
 
 
 elif args.prep_data:
+    # Was used to prepare the loosefiles from the kinetics skeleton data set.
 
+    # Change paths!
     data_paths = [
-        "D:/Temporal Info Graph/kinetics-skeleton/kinetics_train/",
-        "D:/Temporal Info Graph/kinetics-skeleton/kinetics_val/"
+        "local/path/to/kinetics-skeleton/kinetics_train/",
+        "local/path/to/kinetics-skeleton/kinetics_val/"
     ]
 
-    output = "C:/Users/email/Documents/Studium/LMU/5_Semester/Masterthesis/Datasets/Kinetics-skeleton/"
+    # Define output location
+    output = "./"
 
-    log.info("Process data - Class information")
+    log.info("Process data")
 
     _ = TIGDataset(name="kinetics-skeleton", s_files=data_paths, path=output,
                    verbose=True, process_label=True)
 
     log.info("Data set processed")
-
-
-elif args.baseline:
-    log.info(f"Run baseline on {args.data}")
-    data = TIGDataset(name=args.data, path="../content/")
-    data.x = data.x.reshape(data.x.shape[0], -1)
-
-
-    top5 = []
-    top1 = []
-    print("Start baseline training")    
-    for i in trange(10):
-        model = get_model("svm")
-        model.fit(data.x, data.y)
-
-        yhat = model.decision_function(data.x)                
-        # accuracy_score(emb_y, yhat)
-        top5.append(top_k_accuracy_score(data.y, yhat, k = 5))
-        top1.append(top_k_accuracy_score(data.y, yhat, k = 1))
-        print(f"Iteration {i} | Top1: {np.mean(top1)} Top5: {np.mean(top5)} ")
-
-    print("Save baseline accuracies...")
-    np.save("../content/top1_svm.npy", top1)
-    np.save("../content/top5_svm.npy", top5)
-
